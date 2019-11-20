cc45cca35757b8e5e3e3e4ffc742ea3dc88a5727
Abed <aislam360@gmail.com>
2019-11-20 11:49:58 -0500

    initial commit

---
+++ Lab9.java
@@ -0,0 +1,9 @@
+/*
+ Create an application which generates 20 random numbers between 1 and 100 and writes them to a text file on separate lines. 
+ Then the program should read the numbers from the file, calculate the average of the numbers, and display this to the screen.
+*/
+
+ /*
+ Write a recursive method which takes an integer number and returns the sum of the numbers from 1 to that number.
+ The method must solve the problem recursively. Then write an application which calls the method with a few different numbers and displays the return value of the method.
+ */
